bda48fd3938c254523c3c9edbef9e4b3ed8aa8e4
ITSomnathgouda19 <itsomnathgouda19@gmail.com>
2026-02-28 20:20:11 +0530

    Added environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  
+  emailServiceID: 'service_arn8iut',
+  emailTemplateID: 'template_f8qvq0e',
+  emailPublicKey:'OySBO-1CSGuM1s4Hr'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+
+
+
+export const environment = {
+  production: false,
+
+  emailServiceID: 'service_arn8iut',
+  emailTemplateID: 'template_f8qvq0e',
+  emailPublicKey:'OySBO-1CSGuM1s4Hr'
+  
+};
